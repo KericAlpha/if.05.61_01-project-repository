3e2e06f99131dec0c61c4279a2a16091fd2454f6
DWizany04 <d.wizany04@gmail.com>
2021-11-12 12:38:22 +0100

    added RomanNumberConverter code from lession

---
+++ 03_SourceCode/Kata/RomanNumberConverter/Test/Tests/TRoman.java
@@ -0,0 +1,60 @@
+package Tests;
+
+import Source.RomanNumberConverter;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TRoman {
+    @Test
+    void ItShouldReturnIGiven1() {
+        assertEquals("I", RomanNumberConverter.toRoman(1));
+    }
+
+    @Test
+    void ItShouldReturnIIGiven2() {
+        assertEquals("II",RomanNumberConverter.toRoman(2));
+    }
+
+    @Test
+    void ItShouldReturnIIIGiven3() {
+        assertEquals("III",RomanNumberConverter.toRoman(3));
+    }
+
+    @Test
+    void ItShouldReturnVGiven5() {
+        assertEquals("V",RomanNumberConverter.toRoman(5));
+    }
+
+    @Test
+    void ItShouldReturnVIsGivenBetween6and8() {
+        assertEquals("VI",RomanNumberConverter.toRoman(6));
+        assertEquals("VII",RomanNumberConverter.toRoman(7));
+    }
+
+    @Test
+    void ItShouldReturnXGiven10() {
+        assertEquals("X",RomanNumberConverter.toRoman(10));
+    }
+
+    @Test
+    void ItShouldReturnXIGiven11() {
+        assertEquals("XI",RomanNumberConverter.toRoman(11));
+    }
+
+    @Test
+    void ItShouldReturnXXGiven20()  {
+        assertEquals("XX",RomanNumberConverter.toRoman(20));
+    }
+
+    @Test
+    void ItShouldReturnIVGiven4() {
+        assertEquals("IV",RomanNumberConverter.toRoman(4));
+    }
+
+    @Test
+    void ItShouldReturnIXGiven9() {
+        assertEquals("IX",RomanNumberConverter.toRoman(9));
+    }
+}
+
+
---
+++ 03_SourceCode/Kata/RomanNumberConverter/src/Source/RomanNumberConverter.java
@@ -0,0 +1,31 @@
+package Source;
+
+public class RomanNumberConverter {
+
+    public static String toRoman(int aNumber)
+    {
+        String romanNumber = "";
+        for (var numeral : Numeral.values()){
+            while(aNumber >= numeral.mArabic) {
+                romanNumber += numeral.mRoman;
+                aNumber -= numeral.mArabic;
+            }
+        }
+        return romanNumber;
+    }
+    enum Numeral{
+        TEN(10, "X"),
+        NINE(9,"IX"),
+        FIVE(5,"V"),
+        FOUR(4,"IV"),
+        ONE(1,"I");
+
+        private final int mArabic;
+        private final String mRoman;
+
+        Numeral(int arabic, String roman) {
+            mArabic = arabic;
+            mRoman = roman;
+        }
+    }
+}
