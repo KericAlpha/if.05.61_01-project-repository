71aef442926abf95ef4452c5499c0b6a1c78c773
DWizany04 <d.wizany04@gmail.com>
2021-11-19 12:41:21 +0100

    Lession Code

--- 03_SourceCode/Kata/OrderedJobs/Test/OJTest/TOrderedJobs.java
+++ 03_SourceCode/Kata/OrderedJobs/Test/OJTest/TOrderedJobs.java
@@ -1,4 +1,101 @@
 package OJTest;
 
+import OrderedJobs.OrderedJobs;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TOrderedJobs {
+    @Test
+    public void ItshouldReturnAGivenA()
+    {
+        //arrange
+        var sut_ = new OrderedJobs();
+        sut_.registerJob("A");
+        //act
+        sut_.sort();
+        //assert
+        assertEquals("A",sut_.getList());
+    }
+
+    @Test
+    void ItshouldReturnABgivenAB() {
+        //arrange
+        var sut_ = new OrderedJobs();
+        sut_.registerJob("A");
+        sut_.registerJob("B");
+        //act
+        sut_.sort();
+        //assert
+        assertEquals("AB",sut_.getList());
+    }
+
+    @Test
+    void ItShouldReturnABCgivenABC() {
+        //arrange
+        var sut_ = new OrderedJobs();
+        sut_.registerJob("A");
+        sut_.registerJob("B");
+        sut_.registerJob("C");
+        //act
+        sut_.sort();
+        //assert
+        assertEquals("ABC",sut_.getList());
+    }
+
+    @Test
+    void ItShouldReturnABgivenBdebendsOnA() {
+        //arrange
+        var sut_ = new OrderedJobs();
+        sut_.registerJob("B","A");
+
+        sut_.sort();
+
+        assertEquals("AB", sut_.getList());
+    }
+
+    @Test
+    void ItShouldReturnABCGivenCDependsOnBAndBDependsOnC() {
+        //arrange
+        var sut_ = new OrderedJobs();
+        sut_.registerJob("B","A");
+        sut_.registerJob("C","B");
+
+        sut_.sort();
+
+        assertEquals("ABC", sut_.getList());
+    }
+
+    @Test
+    void ItShouldReturnABGivenABB() {
+        var sut_ = new OrderedJobs();
+        sut_.registerJob("A");
+        sut_.registerJob("B");
+        sut_.registerJob("B");
+
+        sut_.sort();
+
+        assertEquals("AB", sut_.getList());
+    }
+
+    @Test
+    void ItShouldReturnABCGivenDdependsOnBandBdependsOnC() {
+        var sut_ = new OrderedJobs();
+        sut_.registerJob("C","B");
+        sut_.registerJob("B","A");
+
+        sut_.sort();
+
+        assertEquals("ABC", sut_.getList());
+    }
+
+    @Test
+    void ItShoudReturnBA_GivenBAUsingSort() {
+        var sut_ = new OrderedJobs();
+        sut_.registerJob("B");
+        sut_.registerJob("A");
+
+        sut_.sort();
+
+        assertEquals("BA", sut_.getList());
+    }
 }
--- 03_SourceCode/Kata/OrderedJobs/src/OrderedJobs/OrderedJobs.java
+++ 03_SourceCode/Kata/OrderedJobs/src/OrderedJobs/OrderedJobs.java
@@ -1,4 +1,45 @@
 package OrderedJobs;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Locale;
+
 public class OrderedJobs {
+
+    private List<Job> jobs = new LinkedList<Job>();
+
+    public void registerJob(String jobName)
+    {
+        if(!jobs.contains(jobName)) {
+            jobs.add(new Job(jobName));
+        }
+    }
+
+    public void registerJob(String dependent,String independent)
+    {
+        Job job = new Job(independent);
+        if(!jobs.contains(job))
+        {
+            jobs.add(job);
+        }
+
+        Job job2 = new Job(dependent,job);
+        if(!jobs.contains(job2))
+        {
+            jobs.add(job2);
+        }
+    }
+
+    public void sort()
+    {
+        char[] arr = jobs.toCharArray();
+        Arrays.sort(arr);
+        jobs = new String(arr);
+    }
+
+    public String getList()
+    {
+        return jobs;
+    }
 }
